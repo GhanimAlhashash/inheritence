8c6d145293ff0e6a4006d1452a4425d0f9af0ac6
Ghanimalhashash <Ghanim-alhashash@hotmail.com>
2024-10-06 11:06:17 +0300

    inhheritance

---
+++ main.dart
@@ -0,0 +1,33 @@
+void main() {
+  final customer = Customer("Khalid");
+  final loyalCustomer = LoyalCustomer('Khalid');
+  customer.add(54.5);
+  customer.add(12.2);
+  print(customer.getPurchaseAmount());
+
+  loyalCustomer.add(50);
+  loyalCustomer.add(50);
+  print(loyalCustomer.getPurchaseAmount());
+}
+
+class Customer {
+  String name;
+  double _purchaseAmount = 0;
+
+  Customer(this.name);
+
+  void add(double price) {
+    _purchaseAmount += price;
+  }
+
+  double getPurchaseAmount() {
+    return _purchaseAmount;
+  }
+}
+
+class LoyalCustomer extends Customer {
+  LoyalCustomer(super.name);
+
+  @override
+  double getPurchaseAmount() => super._purchaseAmount * 0.9;
+}
